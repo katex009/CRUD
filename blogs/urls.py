c8bda8358f81b1e8b67dda1672e3bdc55a03c96f
kate <kate09030506@gmail.com>
2024-03-16 16:51:20 -0400

    create v1

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogCreateView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Publication
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
 class BlogListView(ListView):
     model = Publication
     template_name = 'home.html'
+
+class BlogCreateView(CreateView):
+    model = Publication
+    template_name = 'post_new.html'
+    fields = ['title', 'body', 'author']
